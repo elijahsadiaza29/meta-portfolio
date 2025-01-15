5c866c7cfd1c58debf46074886890a903eeb7cbc
elijahsadiaza29 <elijahsadiaza29@gmail.com>
2025-01-16 00:16:43 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Hero from "./components/pages/page-components/hero-section";
+import Layout from "./layouts/layout";
+
+function App() {
+  return (
+    <Layout>
+      <Hero />
+    </Layout>
+  );
+}
+
+export default App;
